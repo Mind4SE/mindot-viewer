9981510d2c3583067db789aafdb22a8a325be737
Francois DELOYE <francois.deloye@non.schneider-electric.com>
2014-07-07 14:05:52 +0200

    Reworked mindot-viewer launcher to use jar_launcher, added simplifyDot support on Windows

--- src/assemble/resources/bin/mindot-viewer.bat
+++ src/assemble/resources/bin/mindot-viewer.bat
@@ -1,10 +1,24 @@
 
+@REM  Copyright (C) 2014 Schneider Electric
+@REM
+@REM  This file is part of "Mind Compiler" is free software: you can redistribute 
+@REM  it and/or modify it under the terms of the GNU Lesser General Public License 
+@REM  as published by the Free Software Foundation, either version 3 of the 
+@REM  License, or (at your option) any later version.
+@REM 
+@REM  This program is distributed in the hope that it will be useful, but WITHOUT 
+@REM  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+@REM  FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+@REM  details.
+@REM 
+@REM  You should have received a copy of the GNU Lesser General Public License
+@REM  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+@REM 
+@REM  Contact: mind@ow2.org
+@REM 
+@REM  Authors: Schneider Electric Mind4SE
+@REM  Contributors: 
+@REM -----------------------------------------------------------------------------
 @echo off
-
-@REM ==== CHECK MIND_ROOT ===
-@REM use the batch path to determine MIND_ROOT if not defined.
-pushd %~dp0..\
-if "%MIND_ROOT%" == "" set MIND_ROOT=%cd%
-popd
-
-python %MIND_ROOT%\ext\MindViewer.py %*
+@rem echo python %~dp0..\ext\MindViewer.py %*
+python %~dp0..\ext\MindViewer.py %*
